26f6076c48459d686c6edb2024858cf86a7539aa
OrianaC <camussonioriana@gmail.com>
2022-12-10 19:50:49 -0300

    eventos

---
+++ proyecto_integrador_js/eventos.js
@@ -0,0 +1,9 @@
+const btnTotal =document.getElementById ("btnTotalAPagar");
+const btnResultado =document.getElementById ("btnTotalAPagar");
+
+
+function mostrarResultado () {
+console.log ("resultado");
+}
+
+btnTotalAPagar.onclick = mostrarResultado;
