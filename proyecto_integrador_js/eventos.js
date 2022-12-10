22add9ea2bbe0c43ee8d1572edb92b286366b03e
OrianaC <camussonioriana@gmail.com>
2022-12-10 20:10:19 -0300

    variables y constantes

--- proyecto_integrador_js/eventos.js
+++ proyecto_integrador_js/eventos.js
@@ -1,9 +1,4 @@
-const btnTotal =document.getElementById ("btnTotalAPagar");
-const btnResultado =document.getElementById ("btnTotalAPagar");
-
-
-function mostrarResultado () {
-console.log ("resultado");
-}
-
-btnTotalAPagar.onclick = mostrarResultado;
+const precio= 200;
+var descuento1 = precio * 0.80;
+var descuento2 = precio * 0.50;
+var descuento3 = precio * 0.15;
