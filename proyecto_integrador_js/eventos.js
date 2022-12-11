7f11191ea2526e184530464d7161e294922af071
OrianaC <camussonioriana@gmail.com>
2022-12-10 21:08:53 -0300

    ultimos cambios

--- proyecto_integrador_js/eventos.js
+++ proyecto_integrador_js/eventos.js
@@ -1,4 +1,17 @@
-const precio= 200;
-var descuento1 = precio * 0.80;
-var descuento2 = precio * 0.50;
-var descuento3 = precio * 0.15;
+const precio = 200;
+var cantidadDeEntradas = ;
+var descuentoEstudiante = precio * 0.80;
+var descuentoTrainee = precio * 0.50;
+var descuentoJunior = precio * 0.15
+var descuento = precio * categoria;
+var totalAPagar = descuento *cantidadDeEntradas;
+
+function Resumen {
+    totalAPagar = descuento * cantidadDeEntradas
+    var totalAPagar = document.getElementById ("botoncalcular");
+    mostrartotalAPagar.innerHTML= "el total a pagar es" + totalAPagar
+
+}
+
+
+
